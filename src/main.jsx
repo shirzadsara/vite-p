9a1fcc6a53def27687c9f9fda07e9c8e435114de
Sara Shirzad <shirzadsara520@gmail.com>
2025-07-22 20:20:52 +0330

    add some commit

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { store } from './store'
 import router from './router'
 import {RouterProvider } from 'react-router-dom'
 
+//some comment
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
